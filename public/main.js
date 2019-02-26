e657caaf6662ccc3137677645a77f892767a9aec
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-26 21:01:33 +0100

    about section done

--- public/main.js
+++ public/main.js
@@ -4,7 +4,13 @@ const main = () => {
 
 console.log('JS running')
 
-
+  const aboutButton = document.querySelector('.about-button');
+  const aboutText = document.querySelector('.about-text');
+  aboutButton.addEventListener('click', ()=>{
+    console.log('click');
+    aboutText.style.visibility = 'visible';
+    aboutText.style.animation = 'enter-text 2s ease-in';
+  })
 
 }
 
