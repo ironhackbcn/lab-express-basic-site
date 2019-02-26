5a0ee199dbef9690b56c5aa762576f55b55f7150
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-26 21:49:32 +0100

    gallery in progress

--- public/main.js
+++ public/main.js
@@ -6,12 +6,21 @@ console.log('JS running')
 
   const aboutButton = document.querySelector('.about-button');
   const aboutText = document.querySelector('.about-text');
-  aboutButton.addEventListener('click', ()=>{
+  aboutButton.addEventListener('click', function (event){
     console.log('click');
     aboutText.style.visibility = 'visible';
     aboutText.style.animation = 'enter-text 2s ease-in';
   })
 
+  // const image = document.querySelector('#picture');
+  // image.addEventListener('click', () => {
+  //   let source = 'http://localhost:3010/images/cat-one.jpg'
+  //   image.setAttribute.src = source;
+  //   console.log(source)
+  // })
+
+  // console.log(image.src)
+  
 }
 
 window.addEventListener('load', main)
