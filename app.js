41c760dbb3e0808d98c3cde462abdeb030545045
josepeduard <j.e.garriga.obach@gmail.com>
2019-02-27 08:49:24 +0100

    retoque

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/gallery', (req, res) => {
 
 app.use((req, res, next) => {
     res.status(404);
-    res.sendFile(__dirname + '/views/about.html')
+    res.sendFile(__dirname + '/views/error.html')
 });
 
 app.use((err, req, res, next) => {
