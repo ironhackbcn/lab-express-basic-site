797efa8c4d9adb0f5254fc5cbc91a1391ad03fd6
franlol <fraaaaan@gmail.com>
2019-02-27 09:37:44 +0100

    j3j3j3

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const port = 3000;
 app.use(express.static(__dirname + `/public`));
 
 app.get("/", (req, res) => res.sendFile(__dirname + `/views/index.html`));
-app.get("/about", (req, res) => res.sendFile(__dirname + `/views/about.html`));
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + `/views/about.html`)
+});
 app.get("/photo", (req, res) => res.sendFile(__dirname + `/views/photo.html`));
 
+//404 MIDDLEWARE
 app.use((req, res, next) => {
     res.status(404);
-    // console.log(__dirname + `/views/error404.html`)
     res.sendFile(__dirname + `/views/error404.html`);
-    // res.sendFile(__dirname + `/views/error404.html`)
 });
 
-// //404 middleware
-// app.use((req, res, next) => {
-//     res.status(404);
-//     res.send('not-found');
-// });
+//Error middleware
+app.use((err, req, res, next) => {
+    res.status(404);
+    res.send('not-found');
+});
 
 app.listen(port, () => console.log(`Listening ${port}`));
