0aa49af9713531ed60d96a38315b81ec6bcdde69
Scott Mallinson <scott@scottmallinson.com>
2019-04-24 21:35:38 +0200

    Express Basic Website

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next)=> {
+  response.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.get('/about', (request, response, next)=> {
+  response.sendFile(__dirname + '/public/views/about.html');
+});
+
+app.get('/gallery', (request, response, next)=> {
+  response.sendFile(__dirname + '/public/views/gallery.html');
+});
+
+app.get('/*', (request, response, next)=> {
+  response.sendFile(__dirname + '/public/views/404.html');
+});
+
+app.listen(3000, () => {
+  console.log('My first express up is running :) ');
+});
