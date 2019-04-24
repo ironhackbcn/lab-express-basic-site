236db6c6ad9f04c343a4fb2202e54560c9fd1fc8
Eric Capdevila <eric241@hotmail.es>
2019-04-24 22:38:28 +0200

    done the home, error, about and gallery pages, created a button for the home that pops up an image, having trouble with the event listener in the main.js and connecting the css to the html

---
+++ public/main.js
@@ -0,0 +1,7 @@
+let afraidButton = document.getElementById('afraidButton');
+let home = document.getElementById('Halloween');
+
+afraidButton.addEventListener('click', function(){
+  home.innerHTML(`<img src="/public/scaryface.jpg">`);
+ console.log('cat')
+})
