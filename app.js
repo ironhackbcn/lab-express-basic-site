1842ab111658d7987bc42881ed6fe1cabd746da4
Scott Mallinson <scott@scottmallinson.com>
2019-04-25 07:25:52 +0200

    Added appropriate response status codes to requests

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (request, response, next)=> {
+  response.statusCode = 200;
   response.sendFile(__dirname + '/public/views/index.html');
 });
 
 app.get('/about', (request, response, next)=> {
+  response.statusCode = 200;
   response.sendFile(__dirname + '/public/views/about.html');
 });
 
 app.get('/gallery', (request, response, next)=> {
+  response.statusCode = 200;
   response.sendFile(__dirname + '/public/views/gallery.html');
 });
 
 app.get('/*', (request, response, next)=> {
+  response.statusCode = 404;
   response.sendFile(__dirname + '/public/views/404.html');
 });
 
