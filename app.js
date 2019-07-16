0d7761b70d7b18f1a94ecf076a014ba3db228c09
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-16 18:22:11 +0200

    move views directory and change app.js paths

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + '/public/views/home.html')
+  res.sendFile(__dirname + '/views/home.html')
 })
 
 app.get('/about', (req, res, next) => {
-  res.sendFile(__dirname + '/public/views/about.html')
+  res.sendFile(__dirname + '/views/about.html')
 })
 
 app.get('/photo-gallery', (req, res, next) => {
-  res.sendFile(__dirname + '/public/views/photo-gallery.html')
+  res.sendFile(__dirname + '/views/photo-gallery.html')
 })
 
 app.get('/*', (req, res, next) => {
-  res.sendFile(__dirname + '/public/views/not-found.html')
+  res.sendFile(__dirname + '/views/not-found.html')
 })
 
 app.listen(port, () => {
