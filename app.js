add0f6acb1fc4ef8c841245459200f7df6176293
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-16 20:33:13 +0200

    add router t cars models

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.get('/modelX', (req, res, next) => {
   res.sendFile(__dirname + '/views/modelX.html')
 })
 
+app.get('/modelY', (req, res, next) => {
+  res.sendFile(__dirname + '/views/modelY.html')
+})
+
+app.get('/modelS', (req, res, next) => {
+  res.sendFile(__dirname + '/views/modelS.html')
+})
+
+app.get('/model3', (req, res, next) => {
+  res.sendFile(__dirname + '/views/model3.html')
+})
+
 app.get('/*', (req, res, next) => {
   res.sendFile(__dirname + '/views/not-found.html')
 })
