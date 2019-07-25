f169e2722814908d290a206cfe432588de2043db
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-07-25 13:00:47 +0200

    Initial Commit
    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use = express.static("public");
+
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home-page.html");
+  console.log(__dirname);
+});
+
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about-page.html");
+  console.log(__dirname);
+});
+
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/photogallery-page.html");
+  console.log(__dirname);
+});
+
+app.get("/*", (request, response, next) => {
+  response.sendFile(__dirname + "/views/404-page.html");
+  console.log(__dirname);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening at port 3000");
+});
