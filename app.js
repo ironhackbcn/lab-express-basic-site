71bd69bc54add40c84cd17bf3a113db52869d2fc
Olga Zinoveva <ozinka81@gmail.com>
2019-07-28 23:30:39 +0200

    create 2 pages: home and about

---
+++ app.js
@@ -0,0 +1,26 @@
+// Require Express
+const express = require("express");
+
+// Express server handling requests and responses
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static("public"));
+
+//start server
+app.listen(3000, () => {
+  console.log("My first app listening on port 3000!");
+});
+
+// app.get("/", (request, response, next) => {
+//   console.log(request);
+//   response.send("<h1>Parque natural del Montseny </h1>");
+// });
+
+app.get("/", (request, response, next) => {
+  response.sendFile(__dirname + "/views/home-page.html");
+});
+
+app.get("/montseny", (request, response, next) => {
+  response.sendFile(__dirname + "/views/about-page.html");
+});
