4b2c52a4f827c9e501cee1275a836b39f560efa6
Ale Bausa <ale.bausa@gmail.com>
2019-07-29 17:25:11 +0200

    add error 404 page style

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const port = 3000;
 
 app.use(express.static("public"));
 
 //Home
-app.get("/", (request, response, next) => {
+app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/home.html");
 });
 
 //About
-app.get("/about", (request, response, next) => {
+app.get("/about", (request, response) => {
   response.sendFile(__dirname + "/views/about.html");
 });
 
 //Photo
-app.get("/photo", (request, response, next) => {
+app.get("/photo", (request, response) => {
   response.sendFile(__dirname + "/views/photo.html");
 });
 
+//404
+app.get("*", (request, response) => {
+  response.status(404).sendFile(__dirname + "/views/404.html");
+});
+
 //Server started
-app.listen(3000, () => {
-  console.log("My first app listening on port 3000!");
+app.listen(port, () => {
+  console.log(`My app is listening on port ${port}!`);
 });
